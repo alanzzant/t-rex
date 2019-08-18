bede8ba87b3c03906a4844a04387ea55f54a8a1e
Alan Santiago <alanivan.s06@gmail.com>
2019-08-18 18:53:59 -0500

    Add the probability function this is for the cactus apparition 😎

--- src/Utils.js
+++ src/Utils.js
@@ -1,3 +1,14 @@
 export function getRandomValue(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min
 }
+
+export function generateProbability(probability) {
+  let foo = getRandomValue(0, probability)
+  let temp = false
+
+  if(foo == 1)  {
+    temp = true
+  }
+
+  return temp
+}
